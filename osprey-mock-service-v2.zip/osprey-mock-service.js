27283406d32d0633823a71a5811176d7713460a4
Leonardo <leonardo.zanguetin@voxage.com.br>
2019-02-05 12:06:02 -0200

    Initial

---
+++ osprey-mock-service-v2.zip/osprey-mock-service.js
@@ -0,0 +1,209 @@
+var Negotiator = require('negotiator')
+var resources = require('osprey-resources')
+var osprey = require('osprey')
+
+/**
+ * Export the mock server.
+ */
+module.exports = ospreyMockServer
+module.exports.createServer = createServer
+module.exports.createServerFromBaseUri = createServerFromBaseUri
+module.exports.loadFile = loadFile
+
+/**
+ * Create an Osprey server instance.
+ *
+ * @param  {Object}   raml
+ * @return {Function}
+ */
+function ospreyMockServer (raml) {
+  return resources(raml.resources, handler)
+}
+
+/**
+ * Create a server with Osprey and the mock service.
+ *
+ * @param  {Object}   raml
+ * @param  {Object}   options
+ * @return {Function}
+ */
+function createServer (raml, options) {
+  var app = osprey.Router()
+
+  app.use(osprey.server(raml, options))
+  app.use(ospreyMockServer(raml))
+  app.use(osprey.errorHandler())
+
+  return app
+}
+
+/**
+ * Create a mock service using the base uri path.
+ *
+ * @param  {Object}   raml
+ * @param  {Object}   options
+ * @return {Function}
+ */
+function createServerFromBaseUri (raml, options) {
+  var app = osprey.Router()
+  var path = (raml.baseUri || '').replace(/^(\w+:)?\/\/[^/]+/, '') || '/'
+
+  app.use(path, raml.baseUriParameters, createServer(raml, options))
+
+  return app
+}
+
+/**
+ * Create a mock service from a filename.
+ *
+ * @param  {String}   filename
+ * @param  {Object}   options
+ * @return {Function}
+ */
+function loadFile (filename, options) {
+  options = options || {}
+  return require('raml-1-parser')
+    .loadRAML(filename, { rejectOnErrors: true })
+    .then(function (ramlApi) {
+      var raml = ramlApi.expand(true).toJSON({
+        serializeMetadata: false
+      })
+      options['RAMLVersion'] = ramlApi.RAMLVersion()
+      return createServerFromBaseUri(raml, options)
+    })
+}
+
+/**
+ * Parse URL to extract variables
+ */
+function getUrlVars(url) {
+  var vars = {};
+  var parts = url.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) {
+      vars[key] = value;
+  });
+  return vars;
+}
+
+
+/**
+ * Returns either a random example from examples or the single example.
+ *
+ * @param {Object} obj
+ */
+function getSingleExample (obj) {
+  if (obj.examples) {
+    var randomIndex = Math.floor(Math.random() * obj.examples.length)
+    return obj.examples[randomIndex].value || obj.examples[randomIndex]
+  } else {
+    return obj.example
+  }
+}
+
+/**
+ * Create a RAML example method handler.
+ *
+ * @param  {Object}   method
+ * @return {Function}
+ */
+function handler (method) {
+  var statusCode = getStatusCode(method)
+  var response = (method.responses || {})[statusCode] || {}
+  var bodies = response.body || {}
+  var headers = {}
+  var types = Object.keys(bodies)
+
+  // Set up the default response headers.
+  if (response.headers) {
+    Object.keys(response.headers).forEach(function (headerName) {
+      var header = response.headers[headerName]
+      if (header.default) {
+        headers[header.name] = header.default
+      } else if (header.example || header.examples) {
+        var example = getSingleExample(header)
+        headers[header.name] = example
+      }
+    })
+  }
+
+  return function (req, res) {
+    var negotiator = new Negotiator(req)
+    var type = negotiator.mediaType(types)
+    if (req.params && (req.params.mediaTypeExtension || req.params.ext)) {
+      var ext = req.params.mediaTypeExtension || req.params.ext
+      ext = ext.slice(1)
+      type = 'application/' + ext
+    }
+    var body = bodies[type] || {}
+
+    if (body && body.properties) {
+      var propertiesExample = Object.keys(body.properties).reduce(function (example, property) {
+        if (body.properties[property].example) {
+          example[property] = body.properties[property].example
+        }
+        return example
+      }, {})
+    }
+    res.statusCode = statusCode
+    setHeaders(res, headers)
+
+    if (type) {
+      res.setHeader('Content-Type', type)
+      var example = body.example
+
+      // Parse body.examples
+      if (Array.isArray(body.examples)) {
+
+        // if a URL var named __exampleId is found, it is used to find a named Example
+        var exampleId = getUrlVars(req.originalUrl).__exampleId
+        if( exampleId ) {
+          ex = body.examples.find(function (ex) {
+            return( ex.name === exampleId )
+          })
+        }
+        if( exampleId && ex ) {
+          example = ex.structuredValue ? ex.structuredValue : ex
+        } else {
+          var newBody = {}
+          newBody.examples = body.examples.map(function (ex) {
+            if (ex.structuredValue) {
+              return ex.structuredValue
+            } else {
+              return ex
+            }
+          })
+          example = getSingleExample(newBody)
+        }
+      }
+
+      if (example) {
+        res.write(typeof example !== 'string' ? JSON.stringify(example) : example)
+      } else if (propertiesExample) {
+        res.write(typeof propertiesExample === 'object' ? JSON.stringify(propertiesExample) : propertiesExample)
+      }
+    }
+
+    res.end()
+  }
+}
+
+/**
+ * Get an appropriate HTTP response code.
+ *
+ * @param  {Object} method
+ * @return {Number}
+ */
+function getStatusCode (method) {
+  return Object.keys(method.responses || {})[0] || 200
+}
+
+/**
+ * Set a map of headers on the response.
+ *
+ * @param {HTTP.Response} res
+ * @param {Object}        headers
+ */
+function setHeaders (res, headers) {
+  Object.keys(headers).forEach(function (key) {
+    res.setHeader(key, headers[key])
+  })
+}
---
+++ osprey-mock-service-v2.zip/test/test10.js
@@ -0,0 +1,219 @@
+/* global describe, it, before */
+
+var expect = require('chai').expect
+var mockService = require('../')
+var popsicle = require('popsicle')
+var server = require('popsicle-server')
+var finalhandler = require('finalhandler')
+var httpes = require('http')
+var path = require('path')
+
+describe('osprey mock service v1.0', function () {
+  var http
+
+  before(function () {
+    this.timeout(3000)
+    return mockService.loadFile(path.join(__dirname, '/fixtures/example10.raml'), { server: { cors: true, compression: true } })
+      .then(function (raml) {
+        http = httpes.createServer(function (req, res) {
+          return raml(req, res, finalhandler(req, res))
+        })
+      })
+  })
+
+  describe('routes', function () {
+    it('should expose a function', function () {
+      expect(mockService).to.be.a('function')
+    })
+
+    it('should respond with example parameter', function () {
+      return popsicle.default(
+        {
+          method: 'GET',
+          url: '/api/test'
+        }
+      )
+        .use(server(http))
+        .then(function (res) {
+          expect(JSON.parse(res.body)).to.deep.equal({ success: true })
+          expect(res.status).to.equal(200)
+        })
+    })
+
+    it('should respond with nested example parameter', function () {
+      return popsicle.default(
+        {
+          method: 'GET',
+          url: '/api/nested'
+        }
+      )
+        .use(server(http))
+        .then(function (res) {
+          expect(JSON.parse(res.body)).to.deep.equal({ nested: { success: true } })
+          expect(res.status).to.equal(200)
+        })
+    })
+
+    it('should respond with a boolean body', function () {
+      return popsicle.default(
+        {
+          method: 'GET',
+          url: '/api/boolean'
+        }
+      )
+        .use(server(http))
+        .then(function (res) {
+          expect(JSON.parse(res.body)).to.equal(true)
+          expect(res.status).to.equal(200)
+        })
+    })
+
+    it('should respond with multiple examples', function () {
+      return popsicle.default(
+        {
+          method: 'GET',
+          url: '/api/examples'
+        }
+      )
+        .use(server(http))
+        .then(function (res) {
+          var match = /example./.test(JSON.parse(res.body).name)
+          expect(match).to.equal(true)
+          expect(res.status).to.equal(200)
+        })
+    })
+
+    it('should respond to consecutive requests', function () {
+      var req = {
+        method: 'GET',
+        url: '/api/examples'
+      }
+      return popsicle.default(req)
+        .use(server(http))
+        .then(function (res) {
+          popsicle.default(req)
+            .use(server(http))
+            .then(function (res) {
+              var match = /example./.test(JSON.parse(res.body).name)
+              expect(match).to.equal(true)
+              expect(res.status).to.equal(200)
+            })
+        })
+    })
+
+    it('should reject undefined route', function () {
+      return popsicle.default('/api/unknown')
+        .use(server(http))
+        .then(function (res) {
+          expect(res.status).to.equal(404)
+        })
+    })
+
+    it('should have empty body when there are no example property', function () {
+      return popsicle.default('/api/noexample')
+        .use(server(http))
+        .then(function (res) {
+          expect(res.status).to.equal(200)
+          expect(res.body).to.equal('')
+        })
+    })
+
+    it('should return a header \'foo\' equal to the \'default\' value \'test\' instead of the \'example\' value', function () {
+      return popsicle.default(
+        {
+          method: 'GET',
+          url: '/api/headersdefaultbeforeexample'
+        }
+      )
+        .use(server(http))
+        .then(function (res) {
+          expect(res.headers.foo).to.equal('test')
+        })
+    })
+
+    it('should return a header \'foo\' equal to the \'default\' value \'test\'', function () {
+      return popsicle.default(
+        {
+          method: 'GET',
+          url: '/api/headersdefault'
+        }
+      )
+        .use(server(http))
+        .then(function (res) {
+          expect(res.headers.foo).to.equal('test')
+        })
+    })
+
+    it('should return a header \'foo\' equal to the \'example\' value \'bar\'', function () {
+      return popsicle.default(
+        {
+          method: 'GET',
+          url: '/api/headersexample'
+        }
+      )
+        .use(server(http))
+        .then(function (res) {
+          expect(res.headers.foo).to.equal('bar')
+        })
+    })
+
+    it('should return a header \'foo\' equal to any of the \'examples\' value defined', function () {
+      return popsicle.default(
+        {
+          method: 'GET',
+          url: '/api/headersexamples'
+        }
+      )
+        .use(server(http))
+        .then(function (res) {
+          expect(res.headers.foo).to.be.oneOf(['bar', 'foo', 'random', 'another'])
+        })
+    })
+
+    it('should default to document\'s mediatype', function () {
+      return popsicle.default(
+        {
+          method: 'GET',
+          url: '/api/defaultmediatype'
+        }
+      )
+        .use(server(http))
+        .then(function (res) {
+          expect(JSON.parse(res.body))
+            .to.deep.equal({ stringProperty: 'foo', numberProperty: 23 })
+        })
+    })
+
+    it('should respect ext', function () {
+      return popsicle.default(
+        {
+          method: 'GET',
+          url: '/api/ext.json'
+        }
+      )
+        .use(server(http))
+        .then(function (res) {
+          expect(JSON.parse(res.body))
+            .to.deep.equal({ stringProperty: 'foo', numberProperty: 23 })
+        })
+    })
+
+    it('should return property-level examples from type.', function () {
+      return popsicle.default(
+        {
+          method: 'GET',
+          url: '/api/user'
+        }
+      )
+        .use(server(http))
+        .then(function (res) {
+          expect(JSON.parse(res.body).name).to.equal('Kendrick')
+          expect(JSON.parse(res.body).lastname).to.equal('Lamar')
+          expect(JSON.parse(res.body).age).to.equal(10)
+          expect(JSON.parse(res.body).good).to.equal(true)
+          expect(JSON.parse(res.body).array).to.eql(['foo', 'bar'])
+          expect(JSON.parse(res.body).object).to.eql({ 'foo': 1, 'bar': 2 })
+        })
+    })
+  })
+})
